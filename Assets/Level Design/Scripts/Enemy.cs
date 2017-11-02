9c0fffd5cc372d0f3e71f4af96bc9eebc617d44f
Steellion13 <kcbyers1@gmail.com>
2017-11-01 22:58:43 -0700

    1st enemy script
    added entire 1st enemy script

--- Assets/Level Design/Scripts/Enemy.cs
+++ Assets/Level Design/Scripts/Enemy.cs
@@ -5,48 +5,121 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    /*double attackTurnTime = 0.7;
+    float attackTurnTime = 0.7f;
     int rotateSpeed = 120;
     int attackDistance = 17;
     int extraRunTime = 2;
     int damage = 1;
     int attackSpeed = 1;
     int attackRotateSpeed = 20;
-    double idleTime = 1.6;
-    Vector3 puncPosititon = new Vector3((float)0.4, 0, (float)0.7);
-    double punchRadius = 1.1;
+    float idleTime = 1.6f;
+    Vector3 punchPosititon = new Vector3((float)0.4, 0, (float)0.7);
+    float punchRadius = 1.1f;
     AudioClip idleSound;
     AudioClip attackSound;
-    private int attackAngle = 10;
+    private float attackAngle = 10;
     private bool isAttacking = false;
-    private int lastPunchTime = 0;
+    private float lastPunchTime = 0;
     Transform target;
     private CharacterController characterController;
-
-    // Use this for initialization
+    
     void Start ()
+    {
+        StartCoroutine(StartFunction());
+    }
+	
+
+    IEnumerator StartFunction()
     {
         characterController.GetComponent<CharacterController>();
 
         if (!target)
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
-            GetComponent<Animation>().wrapMode = WrapMode.Loop;
-            GetComponent<Animation>().Stop();
         }
-
+        GetComponent<Animation>().wrapMode = WrapMode.Loop;
+        GetComponent<Animation>().Stop();
         GetComponent<AudioSource>().clip = idleSound;
-        //wait for random # of seconds
+        yield return new WaitForSeconds(Random.value);
         while (true)
         {
-            yield Idle();
-            yield Attack();
+            StartCoroutine(Idle());
+            StartCoroutine(Attack());
         }
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    IEnumerator Idle()
     {
-		
-	}*/
+        GetComponent<Animation>().Play("idle");
+        if (idleSound && GetComponent<AudioSource>().clip != idleSound)
+        {
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().clip = idleSound;
+            GetComponent<AudioSource>().loop = true;
+            GetComponent<AudioSource>().Play();
+        }
+        yield return new WaitForSeconds((float)idleTime);
+    }
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+        if (attackSound && GetComponent<AudioSource>().clip != attackSound)
+        {
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().clip = attackSound;
+            GetComponent<AudioSource>().loop = true;
+            GetComponent<AudioSource>().Play();
+        }
+        GetComponent<Animation>().CrossFade("Chase");
+        float angle = 180.0f;
+        float time = 0;
+        Vector3 direction;
+        while (angle > 5 || time < attackTurnTime)
+        {
+            time += Time.deltaTime;
+            angle = Mathf.Abs(Vector3.Angle(gameObject.transform.position, target.position));
+            float move = Mathf.Clamp01((90 - angle) / 90);
+            GetComponent<Animation>()["Chase"].weight = GetComponent<Animation>()["Chase"].speed = move;
+            direction = transform.TransformDirection(Vector3.forward * attackSpeed * move);
+            characterController.SimpleMove(direction);
+            yield break;
+        }
+        float timer = 0.0f;
+        bool lostSight = false;
+        while (timer < extraRunTime)
+        {
+            angle = Vector3.Angle(gameObject.transform.position, target.position);
+            if (Mathf.Abs(angle) > 40)
+            {
+                lostSight = true;
+            }
+            if (lostSight)
+            {
+                timer += Time.deltaTime;
+            }
+            direction = transform.TransformDirection(Vector3.forward * attackSpeed);
+            characterController.SimpleMove(direction);
+            Vector3 pos = transform.TransformPoint(punchPosititon);
+            if (Time.time > lastPunchTime + 0.3 && (pos - target.position).magnitude < punchRadius)
+            {
+                target.SendMessage("ApplyDamage", damage);
+                lastPunchTime = Time.time;
+            }
+            if (characterController.velocity.magnitude < attackSpeed * 0.3)
+            {
+                yield break;
+            }
+        }
+        isAttacking = false;
+        GetComponent<Animation>().CrossFade("idle");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.TransformPoint(punchPosititon), punchRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+    }
 }
