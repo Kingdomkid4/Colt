0ba214c8a3b61f514c8ec35d03b8c926bca4f889
Steellion13 <kcbyers1@gmail.com>
2017-11-07 22:09:57 -0800

    Added 2 enemy scripts

---
+++ Assets/Level Design/Scripts/BodyDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyDestroy : MonoBehaviour {
+
+    Transform deadReplacement;
+    bool DeadBodyGuard = false;
+    float PointValue = 100f;
+
+    void Update()
+    {
+        transform.Find("BodyGuardHead");
+        if (!transform.find("BodyGuardHead"))
+        {
+            DeadBodyGuard = true;
+            GameObject.Find("ScoreDynamic").SendMessageUpwards("NewScore", PointValue, SendMessageOptions.DontRequireReciever);
+            Kill();
+        }
+    }
+
+    void Kill()
+    {
+        if (DeadBodyGuard)
+        {
+            Destroy(gameObject);
+        }
+        if (deadReplacement)
+        {
+            Transform dead = Instatiate(deadReplacement, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Assets/Level Design/Scripts/DamageReciever.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageReciever : MonoBehaviour {
+
+    float hitpoints = 100.0f;
+    float damage = 5.0f;
+    int detonationDelay = 0;
+    Transform explosion;
+    Rigidbody deadReplacement;
+    AudioClip deathSound;
+    ParticleSystem emitter;
+
+    void ApplyDamage(float damage)
+    {
+        if (hitpoints <= 0)
+        {
+            return;
+        }
+        hitpoints -= damage;
+        if (hitpoints <= 0)
+        {
+            ParticleSystem emitter = GetComponentInChildren<ParticleSystem>();
+        }
+        if (emitter)
+        {
+            emitter.Play();
+        }
+        Invoke("DelayedDetonate", detonationDelay);
+    }
+
+    void DelayedDetonate()
+    {
+        BroadcastMessage("Detonate");
+    }
+
+    void Detonate()
+    {
+        Destroy(gameObject);
+        if (explosion)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (deadReplacement)
+        {
+            Rigidbody dead = Instantiate(deadReplacement, transform.position, transform.rotation);
+            dead.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
+            dead.angularVelocity = GetComponent<Rigidbody>().angularVelocity;
+        }
+        ParticleSystem emitter = GetComponentInChildren<ParticleSystem>();
+        if (emitter)
+        {
+            emitter.Stop();
+            emitter.transform.parent = null;
+        }
+    }
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
